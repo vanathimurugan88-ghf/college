ad91c7819d60d70484e35bb77fdf66f2f7f078b2
VANI <vanathimurugan88@gmail.com>
2026-09-23 18:03:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert("Welcome to EduCollege! Explore our departments, courses and events.");
+}
+
+function courseInfo(course) {
+    alert("You selected the " + course + " course.");
+}
